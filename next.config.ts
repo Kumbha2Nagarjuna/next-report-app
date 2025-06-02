604012a92f034a0560ce8d2d91ce45b065003397
Kumbha2Nagarjuna <86190272+Kumbha2Nagarjuna@users.noreply.github.com>
2025-06-02 18:46:58 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export'
+  output: 'export',
+  basePath: '/next-report-app',
+  assetPrefix: '/next-report-app/',
 };
 
 export default nextConfig;
